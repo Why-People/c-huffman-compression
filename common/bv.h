8a6850c3c5903ccd6815519cc2685f2f862cc847
Why-People <matthewha853@gmail.com>
2021-11-08 11:52:11 -0800

    Bit Vector interface + implementation

---
+++ common/bv.h
@@ -0,0 +1,21 @@
+#ifndef __BV_H__
+#define __BV_H__
+
+#include <stdlib.h>
+
+#define BIT_SIZE 8
+#define WORD_SIZE(len) (len / BIT_SIZE + (len % BIT_SIZE ? 1 : 0))
+
+typedef struct BitVector bv_t;
+
+bv_t *bv_init(size_t cap);
+void bv_destroy(bv_t *bv);
+
+void bv_set_bit(bv_t *bv, int index);
+void bv_clr_bit(bv_t *bv, int index);
+int bv_get_bit(bv_t *bv, int index);
+
+size_t bv_len(bv_t *bv);
+size_t bv_cap(bv_t *bv);
+
+#endif
